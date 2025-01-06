6e7a0ef6decca3efc78c66f2f5825c62307c4500
yashrmalhotra <yashrmalhotra11@gmail.com>
2025-01-06 20:21:29 +0530

    rename file

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center fixed top-0 bg-black opacity-15 z-10'>
-        <img src="/loader.gif" alt="" />
+    <div className='w-screen h-screen flex justify-center items-center fixed top-0 bg-black opacity-70 z-10'>
+        <img src="/loader.gif" alt="" className='z-10'/>
     </div>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     host: '0.0.0.0',
     proxy:{
-      "/api":"http://localhost:3000"
+      "/api":"https://avtechfin-za2p.onrender.com"
     }
   }
 })
